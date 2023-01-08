68bc8b9afff51c17152d6551074f109c70c3a0f3
Jakub Szczepanek <kubaszczepanek@interia.pl>
2023-01-08 14:50:58 +0100

    ViewsAndModifiers app

---
+++ 03-ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Jakub Szczepanek on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
