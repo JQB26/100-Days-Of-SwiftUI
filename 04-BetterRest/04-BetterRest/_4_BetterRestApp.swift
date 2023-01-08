9d7a16867e7cecb079e5916805d10703ab228cf6
Jakub Szczepanek <kubaszczepanek@interia.pl>
2023-01-08 15:43:43 +0100

    BetterRest app with ML model

---
+++ 04-BetterRest/04-BetterRest/_4_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_BetterRestApp.swift
+//  04-BetterRest
+//
+//  Created by Jakub Szczepanek on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct _4_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
