79015c7318c0d6745cf1029da79b1bff4e0c21cf
Jakub Szczepanek <kubaszczepanek@interia.pl>
2023-01-08 13:04:05 +0100

    WeSplit app

---
+++ 01-WeSplit/01-WeSplit/_1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_WeSplitApp.swift
+//  01-WeSplit
+//
+//  Created by Jakub Szczepanek on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct _1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
