49b7ca8a1502eab1ddbb90a15de1e03bd87f5e8a
Jakub Szczepanek <kubaszczepanek@interia.pl>
2023-01-08 17:29:10 +0100

    WordScramble app

---
+++ 05-WordScramble/05-WordScramble/_5_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_WordScrambleApp.swift
+//  05-WordScramble
+//
+//  Created by Jakub Szczepanek on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct _5_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
